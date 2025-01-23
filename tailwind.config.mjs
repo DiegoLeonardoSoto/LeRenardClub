4b92f365ae4886a94e180fbdfb57f1fedbe4efca
DiegoLeonardoSoto <leonardo.soto123@hotmail.com>
2025-01-23 17:40:25 -0300

    first

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      textColor: {
+        "yellow-green": "#d7de21",
+      },
+      width: {
+        fill: "-webkit-fill-available",
+      },
+      fontFamily: {
+        dosis: ["Dosis", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        photo1: 'url("src/assets/imgs/psd-ai-2019.jpg")',
+        photo2: 'url("src/assets/imgs/Artboard-10.png")',
+      },
+    },
+  },
+  plugins: [],
+};
